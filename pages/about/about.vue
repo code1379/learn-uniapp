eb93413deb5aeb8295241ab2bc3b45ddd5567687
code1379 <coderxialuo@gmail.com>
2021-06-25 22:10:00 +0800

    添加 gitignore

---
+++ pages/about/about.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		About
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+			console.log("about load")
+		},
+		onTabItemTap(e) {
+			console.log(e)
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		我的
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+			console.log("my load")
+		},
+		onTabItemTap(e) {
+			console.log(e)
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
